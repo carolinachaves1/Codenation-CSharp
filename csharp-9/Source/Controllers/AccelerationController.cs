127453ddd8becd75ec69022df77c6d8278c984d5
Carolina Chaves <carolina.amaralchaves@gmail.com>
2020-07-15 22:11:50 -0300

    Created AccelerationController

--- csharp-9/Source/Controllers/AccelerationController.cs
+++ csharp-9/Source/Controllers/AccelerationController.cs
@@ -12,6 +12,48 @@ namespace Codenation.Challenge.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class AccelerationController : ControllerBase
-    {    
+    {
+        private readonly IAccelerationService _accelerationService;
+        private readonly IMapper _mapper;
+
+        public AccelerationController(IAccelerationService accelerationService, IMapper mapper)
+        {
+            _accelerationService = accelerationService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AccelerationDTO> GetAccelerationById(int id)
+        {
+            var response = _mapper.Map<AccelerationDTO>(_accelerationService.FindById(id));
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public ActionResult<IList<AccelerationDTO>> GetAcceleration(int? companyId = null)
+        {
+            if(companyId == null)
+            {
+                return NoContent();
+            }
+
+            var response = _mapper.Map<List<AccelerationDTO>>(_accelerationService.FindByCompanyId(companyId.Value));
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public ActionResult<AccelerationDTO> Post([FromBody] AccelerationDTO value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var acceleration = _mapper.Map<Acceleration>(value);
+            _accelerationService.Save(acceleration);
+
+            var response = _mapper.Map<AccelerationDTO>(acceleration);
+            return Ok(response);
+        }
     }
 }
