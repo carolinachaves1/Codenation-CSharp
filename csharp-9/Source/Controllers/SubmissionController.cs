37ab99f5f92deade5f5c7c2740c229a363fbe859
Carolina Chaves <carolina.amaralchaves@gmail.com>
2020-07-15 21:11:47 -0300

    Created SubmissionController

--- csharp-9/Source/Controllers/SubmissionController.cs
+++ csharp-9/Source/Controllers/SubmissionController.cs
@@ -13,5 +13,58 @@ namespace Codenation.Challenge.Controllers
     [ApiController]
     public class SubmissionController : ControllerBase
     {
+        private readonly ISubmissionService _submissionService;
+        private readonly IMapper _mapper;
+
+        public SubmissionController(ISubmissionService submissionService, IMapper mapper)
+        {
+            _submissionService = submissionService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<ICollection<SubmissionDTO>> GetSubmission(int? challengeId = null, int? accelerationId = null)
+        {
+            
+            if(challengeId == null && accelerationId == null)
+            {
+                return NoContent();
+            }
+
+            var response = _mapper.Map<List<SubmissionDTO>>(_submissionService.FindByChallengeIdAndAccelerationId(challengeId.Value, accelerationId.Value));
+            return Ok(response);
+        }
+
+        [HttpGet("higherScore")]
+        public ActionResult<decimal> GetHigherScore(int? challengeId = null)
+        {
+            if(challengeId == null)
+            {
+                return NoContent();
+            }
+
+            var response = _submissionService.FindHigherScoreByChallengeId(challengeId.Value);
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public ActionResult<SubmissionDTO> Post([FromBody] SubmissionDTO value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var submission = _mapper.Map<Submission>(value);
+            _submissionService.Save(submission);
+
+            var response = _mapper.Map<SubmissionDTO>(submission);
+
+            return Ok(response);
+        }
+
+
+
     }
 }
