bec2e69162abacd47820d0ee57cf3b09dfc7b7d8
Carolina Chaves <carolina.amaralchaves@gmail.com>
2020-07-15 21:47:56 -0300

    Created ChallengeController

--- csharp-9/Source/Controllers/ChallengeController.cs
+++ csharp-9/Source/Controllers/ChallengeController.cs
@@ -12,5 +12,40 @@ namespace Codenation.Challenge.Controllers
     [ApiController]
     public class ChallengeController : ControllerBase
     {
+        private readonly IChallengeService _challengeService;
+        private readonly IMapper _mapper;
+
+        public ChallengeController(IChallengeService challengeService, IMapper mapper)
+        {
+            _challengeService = challengeService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<List<ChallengeDTO>> GetChallenge(int? accelerationId = null, int? userId = null)
+        {
+            if(accelerationId == null && User == null)
+            {
+                return NoContent();
+            }
+
+            var response = _mapper.Map<List<ChallengeDTO>>(_challengeService.FindByAccelerationIdAndUserId(accelerationId.Value, userId.Value));
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public ActionResult<ChallengeDTO> Post([FromBody] ChallengeDTO value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var challenge = _mapper.Map<Models.Challenge>(value);
+            _challengeService.Save(challenge);
+
+            var response = _mapper.Map<ChallengeDTO>(challenge);
+            return Ok(response);
+        }
     }
 }
