0e593c95478a4d274be9f72822d2c84426857982
Carolina Chaves <carolina.amaralchaves@gmail.com>
2020-07-15 21:38:08 -0300

    Created CompanyController

--- csharp-9/Source/Controllers/CompanyController.cs
+++ csharp-9/Source/Controllers/CompanyController.cs
@@ -12,6 +12,38 @@ namespace Codenation.Challenge.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class CompanyController : ControllerBase
-    {    
+    {
+        private readonly ICompanyService _companyService;
+        private readonly IMapper _mapper;
+
+        public CompanyController(ICompanyService companyService, IMapper mapper)
+        {
+            _companyService = companyService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("id")]
+        public ActionResult<CompanyDTO> GetCompanyById(int id)
+        {
+            var response = _mapper.Map<CompanyDTO>(_companyService.FindById(id));
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public ActionResult<List<CompanyDTO>> GetCompany(int? accelerationId = null, int? userId = null)
+        {
+            if (accelerationId.HasValue && userId == null)
+            {
+                var response = _mapper.Map<CompanyDTO>(_companyService.FindByAccelerationId(accelerationId.Value));
+                return Ok(response);
+            }
+            else if(accelerationId == null && userId.HasValue)
+            {
+                var response = _mapper.Map<CompanyDTO>(_companyService.FindById(userId.Value));
+                return Ok(response);
+            }
+
+            return NoContent();
+        }
     }
 }
