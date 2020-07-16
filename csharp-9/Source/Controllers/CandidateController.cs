d46d6bde13aeb48c821dc44aea8173cf91ddea93
Carolina Chaves <carolina.amaralchaves@gmail.com>
2020-07-15 22:04:29 -0300

    Created CandidateController

--- csharp-9/Source/Controllers/CandidateController.cs
+++ csharp-9/Source/Controllers/CandidateController.cs
@@ -13,5 +13,52 @@ namespace Codenation.Challenge.Controllers
     [ApiController]
     public class CandidateController : ControllerBase
     {
+        private readonly ICandidateService _candidateService;
+        private readonly IMapper _mapper;
+
+        public CandidateController(ICandidateService candidateService, IMapper mapper)
+        {
+            _candidateService = candidateService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{userId}/{accelerationId}/{companyId}")]
+        public ActionResult<CandidateDTO> GetCandidateById(int userId, int accelerationId, int companyId)
+        {
+            var response = _mapper.Map<CandidateDTO>(_candidateService.FindById(userId, accelerationId, companyId));
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public ActionResult<IList<CandidateDTO>> GetCandidate(int? companyId = null, int? accelerationId = null)
+        {
+            if(companyId.HasValue && accelerationId == null)
+            {
+                var response = _mapper.Map<List<CandidateDTO>>(_candidateService.FindByCompanyId(companyId.Value));
+                return Ok(response);
+            }
+            else if(companyId == null && accelerationId.HasValue)
+            {
+                var response = _mapper.Map<List<CandidateDTO>>(_candidateService.FindByAccelerationId(accelerationId.Value));
+                return Ok(response);
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public ActionResult<CandidateDTO> Post([FromBody] CandidateDTO value)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var candidate = _mapper.Map<Candidate>(value);
+            _candidateService.Save(candidate);
+
+            var response = _mapper.Map<CandidateDTO>(candidate);
+            return Ok(response);
+        }
     }
 }
