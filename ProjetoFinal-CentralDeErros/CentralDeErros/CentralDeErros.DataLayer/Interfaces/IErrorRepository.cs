dca331499195f5d573e5b11799103793a524e6e0
Carolina Chaves <carolina.amaralchaves@gmail.com>
2020-08-08 14:00:34 -0300

    Added Interfaces

---
+++ ProjetoFinal-CentralDeErros/CentralDeErros/CentralDeErros.DataLayer/Interfaces/IErrorRepository.cs
@@ -0,0 +1,14 @@
+﻿using CentralDeErros.DataLayer.Models;
+using System.Collections.Generic;
+
+namespace CentralDeErros.DataLayer.Interfaces
+{
+    public interface IErrorRepository
+    {
+        IEnumerable<Error> GetAll();
+        Error GetById(int id);
+        IEnumerable<Error> GetByLevel(string level);
+        IEnumerable<Error> GetByCategory(string category);
+        int Save(Error error);
+    }
+}
